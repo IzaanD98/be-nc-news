7a6e7ec792c9084ecb6a95e04880f6402ea12e9b
IzaanD98 <izzy.dalvi@gmail.com>
2023-02-20 13:49:35 +0000

    added task 3 GET /api/topics

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { fetchAllTopics } = require("./controllers/newsController");
+const {
+  handle500StatusCodes,
+} = require("./errorHandlingControllers/errorController");
+
+const app = express();
+
+app.use(express());
+
+app.get("/api/topics", fetchAllTopics);
+
+app.use(handle500StatusCodes);
+
+module.exports = app;
