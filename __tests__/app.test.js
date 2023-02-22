78cbd6ab591947ec943db9a8e7c92babe6f55c05
IzaanD98 <izzy.dalvi@gmail.com>
2023-02-22 09:54:57 +0000

    added task 11 /api/articles/:article_id (comment count)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -70,7 +70,7 @@ describe("app", () => {
     });
   });
   describe("/api/articles/:article_id", () => {
-    it("200: GET - responds with an article object which contains the correct_id and the correct properties", () => {
+    it("200: GET - responds with an article object which contains the correct_id and the correct properties and includes comment_count based on article_id", () => {
       return request(app)
         .get("/api/articles/1")
         .expect(200)
@@ -84,6 +84,7 @@ describe("app", () => {
             expect(article).toHaveProperty("topic", expect.any(String));
             expect(article).toHaveProperty("created_at", expect.any(String));
             expect(article).toHaveProperty("votes", expect.any(Number));
+            expect(article).toHaveProperty("comment_count", expect.any(String));
             expect(article).toHaveProperty(
               "article_img_url",
               expect.any(String)
@@ -96,18 +97,18 @@ describe("app", () => {
       return request(app)
         .get("/api/articles/banana")
         .expect(400)
-        .then((body) => {
-          const { statusCode } = body;
-          expect(statusCode).toBe(400);
+        .then(({ body }) => {
+          const error = body.message;
+          expect(error).toBe("Bad Request");
         });
     });
     it("404: GET -  responds with 404 status code of Not Found when given a article_id which doesn't exist", () => {
       return request(app)
         .get("/api/articles/1000")
         .expect(404)
-        .then((body) => {
-          const { statusCode } = body;
-          expect(statusCode).toBe(404);
+        .then(({ body }) => {
+          const error = body.message;
+          expect(error).toBe("Article not found");
         });
     });
   });
